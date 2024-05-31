8dc36704cfb8f226a2267ffbfe0689bcdf55afe9
Bang210 <bangvelope@gmail.com>
2024-05-31 14:49:53 +0900

    refactor: entity

--- src/main/java/com/example/demo/dto/CrawlingDto.java
+++ src/main/java/com/example/demo/dto/CrawlingDto.java
@@ -10,10 +10,12 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class CrawlingDto {
+    private long rawDataId;
     private String rawData;
     private LocalDateTime createdTime;
 
     public CrawlingDto(Crawling crawling) {
+        this.rawDataId = crawling.getCrawlingId();
         this.rawData = crawling.getRawData();
         this.createdTime = crawling.getCreatedTime();
     }
