86b8d63c9cbcbbd326d691665572fa03524df1d4
tallguy188 <cksghd4220@naver.com>
2024-06-19 14:36:00 +0900

    setting

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
-@EnableEurekaServer
 @SpringBootApplication
 public class DemoApplication {
 
